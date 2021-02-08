5200df24bad5c575d28b16f22aff1924498694f2
eduardonk9999 <thiago.novak7@gmail.com>
2021-02-08 00:25:26 -0300

    Add, posts facebook

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+import './Header.css';
+
+
+function Header() {
+  return(
+    <header>
+      <img src="https://i.imgur.com/KDIDiSE.png" />
+
+      <nav>
+        <a href="">Meu perfil</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
